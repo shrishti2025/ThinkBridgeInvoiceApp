8acfafb1df8d965e087e2fce7a52216470512f2c
shrishti2025 <jainshrishti1002@gmail.com>
2026-03-14 15:12:43 +0530

    Invoice app

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Invoice API", Version = "v1" });
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Invoice API V1");
+    });
+}
+
 app.MapControllers();
 app.Run();
--- wwwroot/script.js
+++ wwwroot/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/api/invoice')
+    fetch('../API/api/invoice') 
         .then(resp => resp.json())
         .then(data => {
             let html = '<ul>';
